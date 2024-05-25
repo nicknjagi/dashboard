a0d52871ac3622b9ea2eafb930a412d46ec4dd17
Nick <njaginicholas@gmail.com>
2024-05-25 19:32:43 +0300

    update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,32 +12,38 @@ export default function Nav({}:Props) {
   return (
     <div className="border-b border-gold/50 hidden md:block">
       <Navbar>
-      <NavbarContent as="div" justify="end">
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
-            </DropdownItem>
-            <DropdownItem key="profile">Profile</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
-    </Navbar>
+        <NavbarContent as="div" justify="end">
+          <Dropdown
+            classNames={{
+              base: "before:bg-default-200 ", // change arrow background
+              content: "p-0 border-divider bg-background rounded-lg",
+            }}
+            placement="bottom-end"
+          >
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name="Jason Hughes"
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2" textValue="sign in as">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">zoey@example.com</p>
+              </DropdownItem>
+              <DropdownItem key="profile-nav" textValue="profile">Profile</DropdownItem>
+              <DropdownItem key="logout" color="danger" textValue="log out">
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
     </div>
   );
 }
