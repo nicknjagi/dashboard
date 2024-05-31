f06993e4e7496e0a50d0d1d36e4f81f7d9c0fb5a
Nick <njaginicholas@gmail.com>
2024-05-31 21:50:21 +0300

    update route.ts

--- app/api/validate-token/route.ts
+++ app/api/validate-token/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
 import jwt from 'jsonwebtoken';
+import {  generateUniqueToken } from '@/app/lib/auth';
+import nodemailer from "nodemailer";
+import { generateHtml } from '@/components/emailTemplates/email';
 
 export async function GET(req: NextRequest) {
   // Get the token from the query parameters
   const token = req.nextUrl.searchParams.get('token');
   const email = req.nextUrl.searchParams.get('email');
-  const jwtSecret = process.env.JWT_SECRET!;
-console.log(email);
+  const jwtSecret = process.env.JWT_SECRET!
 
   try {
     // Verify the token using the JWT secret
@@ -17,11 +19,53 @@ console.log(email);
       return NextResponse.json({ message: 'Invalid token' });
     }
     else{
-      return NextResponse.redirect(`${req.nextUrl.origin}/create-account`);
+      const cookieName = 'jwt_token';
+      const cookieValue = token as string;
+
+      // Set the cookie to expire in 1 hour 
+      const expiry = new Date();
+      expiry.setTime(expiry.getTime() + 60 * 60 * 1000);
+
+      const response = NextResponse.redirect(`${req.nextUrl.origin}/create-account`);
+
+      // Set the cookie in the response
+      response.cookies.set(cookieName, cookieValue, { expires: expiry, httpOnly: true });
+
+      return response;
     }
   } catch (err) {
-    // console.log(err);
+    console.log(err);
     return NextResponse.json({ message: 'Invalid or expired token' });
+  }
+}
+
+
+export async function POST(req: NextRequest) {
+  const body = await req.json(); // Parse the JSON body
+  const {email} = body
+
+  const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+  });
+
+  const token = generateUniqueToken(email)
+
+  try {
+    const sendResult = await transporter.sendMail({
+      from: SMTP_EMAIL,
+      to:email,
+      subject:'test mail',
+      html:generateHtml(email, token)
+    });
+    return NextResponse.json({ message: 'success' });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: 'error' });
   }
 }
 
