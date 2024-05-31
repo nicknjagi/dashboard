12849b619c44ab98c2f930ddb76b4214f54ce0a7
Nick <njaginicholas@gmail.com>
2024-05-31 21:49:36 +0300

    feat: create sendTokenModal.tsx

---
+++ components/modals/sendTokenModal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/modal";
+import SendTokenForm from "../forms/sendTokenForm";
+
+type Props = {};
+
+export default function SendTokenModal({}: Props) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        size="sm"
+        onPress={onOpen}
+        className="bg-cultured text-forrestGreen hover:bg-opacity-90 font-medium"
+      >
+        Send Invite
+      </Button>
+      <Modal
+        className="bg-forrestGreen"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Send Invite
+              </ModalHeader>
+              <ModalBody>
+                <SendTokenForm onClose={onClose} />
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
