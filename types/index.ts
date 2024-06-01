6d04d521ab71248faffec971c61385ba01c9989d
Nick <njaginicholas@gmail.com>
2024-06-01 16:49:35 +0300

    update types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,16 @@
 import {SVGProps} from "react";
+import { z } from "zod";
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+export const SignUpSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters."),
+  passwordConfirm: z.string()
+}).refine(data => data.password === data.passwordConfirm, {
+  message:"Passwords must match",
+  path:["passwordConfirm"]
+})
+
+export type TSignUpSchema = z.infer<typeof SignUpSchema>
