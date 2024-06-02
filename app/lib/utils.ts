6c6da265d22d3f3a36b2013f687cb906d3639662
Nick <njaginicholas@gmail.com>
2024-06-02 16:36:07 +0300

    update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -47,38 +47,6 @@ export async function logout(){
   }
 }
 
-// async function createRecord(
-//   token: string,
-//   collectionName: string,
-//   data: object
-// ) {
-//   const response = await axios.post(
-//     `${BASE_URL}/collections/${collectionName}/records`,
-//     data,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     }
-//   );
-//   return response.data;
-// }
-
-
-// (async () => {
-//   const email = 'user@example.com';
-//   const password = 'password123';
-//   // Register a new user
-//   await registerUser(email, password);
-//   // Login and get a token
-//   const token = await loginUser(email, password);
-//   // Create a new record
-//   const newRecord = await createRecord(token, 'myCollection', { field1: 'value1', field2: 'value2'
-// });
-// console.log('Created Record:', newRecord);
-// })();
-
-
 export async function sendEmail(email:string) {
   const url = 'http://localhost:3000/api/validate-token'
   try {
