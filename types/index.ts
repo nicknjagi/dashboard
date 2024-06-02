11c0e595e32035ede3271012f8fc2094e105766e
Nick <njaginicholas@gmail.com>
2024-06-02 16:50:33 +0300

    update types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -42,3 +42,55 @@ export type User = {
   AccountType: 'ADMIN' | 'FACILITATOR'
 };
 
+export type Account = {
+  id: string;
+  collectionId: string;
+  collectionName: string;
+  created: string; // ISO 8601 date string
+  updated: string; // ISO 8601 date string
+  userId: string;
+  active: boolean;
+  subscription_type: 'STUDENT' | 'PRODUCTIVITY'| 'BLENDED'
+  valid_until: string; // ISO 8601 date string
+  workspace_theme: string; // Assuming this is a relation record ID
+  sub_code: string;
+};
+
+export type LibraryItem = {
+  id: string;
+  collectionId: string;
+  collectionName: string;
+  created: string; // ISO 8601 date string
+  updated: string; // ISO 8601 date string
+  Name: string;
+  description: string;
+  link: string;
+  type: 'VIDEO' | 'MUSIC'| 'FILE'
+  thumbnail: string;
+  content: string; // Assuming this is rich text format
+};
+
+export type Workspace = {
+  id: string;
+  collectionId: string;
+  collectionName: string;
+  created: string; // ISO 8601 date string
+  updated: string; // ISO 8601 date string
+  name: string;
+  active: boolean;
+  users: string[]; // Array of account relation record IDs
+  facilitator: string; // Single relation record ID
+};
+
+export type Session = {
+  id: string;
+  collectionId: string;
+  collectionName: string;
+  created: string; // ISO 8601 date string
+  updated: string; // ISO 8601 date string
+  workspace: string; // Relation record ID
+  date: string; // ISO 8601 date string
+  type: 'MEETING' | 'DEEP_FOCUS' | 'NETWORKING' | 'LESSON' | 'COLLABORATION' | 'PUBLIC';
+  link_to_session: string;
+  duration_in_hours: number;
+}
