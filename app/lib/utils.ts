18914021fc1e82e579d9673539c6939bc2a16e2b
Nick <njaginicholas@gmail.com>
2024-06-03 07:40:11 +0300

    update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,3 @@
-
-
-import { TLoginSchema, TSignUpSchema } from "@/types";
 import axios from "axios";
 import PocketBase from 'pocketbase'
 
@@ -8,43 +5,6 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 // const BASE_URL = 'https://mymindcapsule.pockethost.io';
 
 export const pb = new PocketBase(BASE_URL);
-
-export async function registerUser(user: TSignUpSchema) {
-  try {
-    const record = await pb.collection('users').create(user);
-    return {success: true}
-  } catch (error) {
-    console.error('Error registering user:', error);
-    throw error;
-  }
-}
-
-export async function loginUser(user: TLoginSchema) {  
-  try {
-    const response = await axios.post('/api/auth/login', {
-      user,
-    });    
-    const authData = response.data.data;
-     // Save auth data in pb.authStore
-    pb.authStore.save(authData.token, authData.record);
-    return response.data.data;
-  } catch (error) {
-    console.error('Error trying to login:', error);
-    throw new Error('Error trying to login')
-  }
-}
-
-export async function logout(){
-  try {
-    const response = await axios.get('/api/auth/logout');
-    // Clear PocketBase auth store
-    pb.authStore.clear();
-    return {success:true}
-  } catch (error) {
-    console.error('Error logging out:', error);
-    throw new Error('Error logging out')
-  }
-}
 
 export async function sendEmail(email:string) {
   try {
