f3f6d09f19f9f86a1a02d22959669cae7d3e7f4e
Nick <njaginicholas@gmail.com>
2024-06-03 07:39:55 +0300

    update auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,49 @@
+import { TLoginSchema, TSignUpSchema } from '@/types';
+import axios from 'axios';
 import jwt from 'jsonwebtoken';
 import { v4 as uuidv4 } from 'uuid';
+import { pb } from './utils';
 
 export function generateUniqueToken(email:string) {
   const userId = uuidv4()
   const { JWT_SECRET } = process.env  
   const token = jwt.sign({ userId, email }, JWT_SECRET as string, { expiresIn: '1h' }); 
   return token;
+}
+
+export async function registerUser(user: TSignUpSchema) {
+  try {
+    const record = await pb.collection('users').create(user);
+    return {success: true}
+  } catch (error) {
+    console.error('Error registering user:', error);
+    throw error;
+  }
+}
+
+export async function loginUser(user: TLoginSchema) {  
+  try {
+    const response = await axios.post('/api/auth/login', {
+      user,
+    });    
+    const authData = response.data.data;
+     // Save auth data in pb.authStore
+    pb.authStore.save(authData.token, authData.record);
+    return response.data.data;
+  } catch (error) {
+    console.error('Error trying to login:', error);
+    throw new Error('Error trying to login')
+  }
+}
+
+export async function logout(){
+  try {
+    const response = await axios.get('/api/auth/logout');
+    // Clear PocketBase auth store
+    pb.authStore.clear();
+    return {success:true}
+  } catch (error) {
+    console.error('Error logging out:', error);
+    throw new Error('Error logging out')
+  }
 }
