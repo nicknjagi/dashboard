c84d2cd8b271c9c06df3fce6f7326356706aa3cf
Nick <njaginicholas@gmail.com>
2024-06-03 17:38:24 +0300

    update facilitatorsList.tsx

--- components/facilitatorsList.tsx
+++ components/facilitatorsList.tsx
@@ -1,13 +1,21 @@
-'use client'
+"use client";
 
 import React, { useEffect, useState } from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/table";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/table";
 import { User } from "@/types";
 import { fetchFacilitators } from "@/app/lib/utils";
 import { BadgeCheck } from "lucide-react";
 import Loading from "./loading";
 
-type Props = {}
+type Props = {};
 
 const columns = [
   {
@@ -29,12 +37,12 @@ const columns = [
 ];
 
 export default function FacilitatorsList({}: Props) {
-  const [users, setUsers] = useState<User[]>([])
+  const [users, setUsers] = useState<User[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(()=>{
-    async function getUsers(){
+  useEffect(() => {
+    async function getUsers() {
       try {
         const data = await fetchFacilitators();
         setUsers(data.items);
@@ -45,52 +53,55 @@ export default function FacilitatorsList({}: Props) {
       }
     }
     getUsers();
-  }, [])
+  }, []);
 
-  if(isLoading){
-    return <Loading />
+  if (isLoading) {
+    return <Loading />;
   }
 
-  if(error){
-    return <p>{error}</p>
+  if (error) {
+    return <p>{error}</p>;
   }
 
   return (
-    <div className="mt-6 overflow-x-auto rounded-lg">
+    <div className="mt-6 overflow-x-auto rounded-2xl shadow-md">
       <Table
         // isStriped
         classNames={{
-          wrapper: ["bg-background border border-cultured/10 w-full min-w-[640px]"],
-          th:['bg-cultured/10 ']
+          wrapper: [
+            "bg-background border border-cultured/10 w-full min-w-[640px]",
+          ],
+          th: ["bg-cultured/10 "],
         }}
-        aria-label="Example static collection table"
+        aria-label="Facilitators list"
       >
         <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={users}>
-        {(item) => (
-          <TableRow key={item.username}>
-            {(columnKey) => <TableCell>
-              {columnKey === 'verified' ? (
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={users}>
+          {(item) => (
+            <TableRow key={item.username}>
+              {(columnKey) => (
+                <TableCell>
+                  {columnKey === "verified" ? (
                     item.verified ? (
                       <span className="flex justify-center lg:justify-start">
                         <BadgeCheck color="#48b446" />
                       </span>
                     ) : (
-                      <span className="block text-center lg:text-start">
-                        -
-                      </span>
+                      <span className="block text-center lg:text-start">-</span>
                     )
                   ) : (
                     getKeyValue(item, columnKey)
                   )}
-            </TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
+                </TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
       </Table>
     </div>
-  )
+  );
 }
-
