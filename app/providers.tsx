1907d202da3233c7a3328de33b38033d07004d1e
Nick <njaginicholas@gmail.com>
2024-06-07 20:12:47 +0300

    update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,21 +2,28 @@
 
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export interface ProvidersProps {
-	children: React.ReactNode;
-	themeProps?: ThemeProviderProps;
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
 }
+
+const queryClient = new QueryClient();
 
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
-	return (
-		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-		</NextUIProvider>
-	);
+  return (
+    <NextUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
