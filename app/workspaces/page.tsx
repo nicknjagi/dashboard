544973693de1872b0ad0ac891c7d39f444f5572c
Nick <njaginicholas@gmail.com>
2024-06-07 20:13:53 +0300

    update page.tsx

--- app/workspaces/page.tsx
+++ app/workspaces/page.tsx
@@ -1,6 +1,15 @@
+import CreateWorkspaceModal from "@/components/modals/createWorkspaceModal"
+import WorkspacesList from "@/components/workspacesList"
+
 type Props = {}
 export default function Workspaces({}: Props) {
   return (
-    <div>Workspaces</div>
+    <section className="py-4">
+      <div className="flex justify-between items-center">
+        <h1 className={"text-xl md:text-2xl font-medium"}>Workspaces</h1>
+        <CreateWorkspaceModal />
+      </div>
+      <WorkspacesList />
+    </section>
   )
 }
