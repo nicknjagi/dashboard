a4fc3fa8f3d3729ef9e888e5870e3bfecb46c452
Nick <njaginicholas@gmail.com>
2024-06-07 12:18:17 +0300

    update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import PocketBase from 'pocketbase'
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 // const BASE_URL = 'https://mymindcapsule.pockethost.io';
 
 export const pb = new PocketBase(BASE_URL);
 
-export async function sendEmail(email:string) {
+export async function sendEmail(email: string) {
   try {
     const response = await axios.post(`/api/validate-token`, {
       email,
@@ -17,19 +17,4 @@ export async function sendEmail(email:string) {
     throw new Error('Error sending invite')
   }
 }
-
-export async function fetchFacilitators(){
-  try {
-    const response = await axios.get(`${BASE_URL}/api/collections/users/records?filter=(AccountType='FACILITATOR')`, {
-      headers: {
-        Authorization: `Bearer ${pb.authStore.token}`
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-    throw new Error('Error fetching users')
-  }
-}
-
 
