1550970303b2328e56c137f2245d82d63c59ed95
Nick <njaginicholas@gmail.com>
2024-06-07 12:18:29 +0300

    feat: create workspaces.ts

---
+++ app/lib/workspaces.ts
@@ -0,0 +1,78 @@
+
+import { Workspace, User } from "@/types";
+import { BASE_URL, pb } from "./utils";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export async function getWorkspace(id: string) {
+  try {
+    const url = `${BASE_URL}/api/collections/workspaces/records/${id}`
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${pb.authStore.token}`
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error fetching workspace:', error);
+    throw new Error('Error fetching workspace')
+  }
+}
+
+export async function getWorkspaces() {
+  try {
+    const url = `${BASE_URL}/api/collections/workspaces/records?sort=-created`
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${pb.authStore.token}`
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error fetching workspaces:', error);
+    throw new Error('Error fetching workspaces')
+  }
+}
+
+export async function createWorkspace(data: Workspace) {
+  const user = pb.authStore.model as User
+
+  if (user.AccountType !== 'ADMIN') {
+    toast.error('Not authorized')
+    return
+  }
+
+  try {
+    const url = `${BASE_URL}/api/collections/workspaces/records`
+    const response = await axios.patch(url, data, {
+      headers: {
+        Authorization: `Bearer ${pb.authStore.token}`
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error creating workspace:', error);
+    throw new Error('Error creating workspace')
+  }
+}
+export async function updateWorkspace(data: Workspace) {
+  const user = pb.authStore.model as User
+
+  if (user.AccountType !== 'ADMIN') {
+    toast.error('Not authorized')
+    return
+  }
+
+  try {
+    const url = `${BASE_URL}/api/collections/workspaces/records/${data.id}`
+    const response = await axios.patch(url, data, {
+      headers: {
+        Authorization: `Bearer ${pb.authStore.token}`
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error updating account:', error);
+    throw new Error('Error updating account')
+  }
+}
