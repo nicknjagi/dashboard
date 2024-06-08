2a92256b07b2051b3b83279432a5574c25e405e3
Nick <njaginicholas@gmail.com>
2024-06-08 20:31:19 +0300

    update workspacesList.tsx

--- components/workspacesList.tsx
+++ components/workspacesList.tsx
@@ -1,102 +1,77 @@
 "use client";
 
 import { getWorkspaces } from "@/app/lib/workspaces";
-import { Workspace } from "@/types";
+import { User, Workspace } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "./loading";
-import { Tooltip } from "@nextui-org/tooltip";
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  getKeyValue,
-} from "@nextui-org/table";
-import { Button } from "@nextui-org/button";
-import {Chip} from "@nextui-org/chip";
+import { Chip } from "@nextui-org/chip";
 import UpdateWorkspaceModal from "./modals/updateWorkspaceModal";
-
-const columns = [
-  {
-    key: "name",
-    label: "NAME",
-  },
-  {
-    key: "status",
-    label: "STATUS",
-  },
-  {
-    key: "users",
-    label: "USERS",
-  },
-  {
-    key: "actions",
-    label: "ACTIONS",
-  },
-  // {
-  //   key: "created",
-  //   label: "CREATED",
-  // },
-];
+import DeleteWorkspaceModal from "./modals/deleteWorkspaceModal";
+import { ChevronRight, UsersRound } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { pb } from "@/app/lib/utils";
 
 type Props = {};
 
 export default function WorkspacesList({}: Props) {
+  const [userModel, setUserModel] = useState<User | null>(null);
   const { data, error, isLoading } = useQuery({
     queryKey: ["workspaces"],
     queryFn: getWorkspaces,
   });
 
+  useEffect(() => {
+    const user = pb.authStore.model as User;
+    setUserModel(user);
+  }, []);
+
   if (isLoading) return <Loading />;
   if (error) return <p className="mt-10">Error loading workspaces</p>;
 
   return (
-    <div className="mt-6 overflow-x-auto rounded-2xl shadow-md max-w-fit">
-      <Table
-        // isStriped
-        classNames={{
-          wrapper: [
-            "bg-background border border-cultured/10 w-full min-w-[640px] max-w-fit",
-          ],
-          th: ["bg-cultured/10 "],
-        }}
-        aria-label="Facilitators list"
-      >
-        <TableHeader columns={columns}>
-          {(column) => (
-            <TableColumn key={column.key}>{column.label}</TableColumn>
-          )}
-        </TableHeader>
-        {data.items?.length > 0 ? (
-          <TableBody items={data.items}>
-            {(workspace: Workspace) => (
-              <TableRow key={workspace.id}>
-                {(columnKey) => (
-                  <TableCell>
-                    {columnKey === "actions" ? (
-                      <UpdateWorkspaceModal workspace={workspace}/>
-                    ) : columnKey === "status" ? (
-                      workspace.active ? (
-                        <Chip color="success" size="sm">Active</Chip>
-                      ) : (
-                        <Chip size="sm">Inactive</Chip>
-                      )
-                    ) : columnKey === "users"? (
-                      <span>{workspace.users.length}</span>
-                    ): (
-                      getKeyValue(workspace, columnKey)
-                    )}
-                  </TableCell>
-                )}
-              </TableRow>
-            )}
-          </TableBody>
-        ) : (
-          <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
-        )}
-      </Table>
-    </div>
+    <section className="mt-6 flex flex-wrap justify-center md:justify-start gap-4 md:gap-6">
+      {data.items.map((workspace: Workspace) => {
+        return (
+          <div
+            key={workspace.id}
+            className="pt-4 md:pt-6 w-full max-w-sm border shadow border-cultured/20 rounded-lg"
+          >
+            <h2 className="mb-6 text-xl px-4 md:px-6">{workspace.name}</h2>
+
+            <div className="flex justify-between px-4 md:px-6 mb-4">
+              <div className="flex gap-1 items-center">
+                <UsersRound size={18} />
+                <span className="text-sm">
+                  Users <Chip size="sm">{workspace.users.length}</Chip>
+                </span>
+              </div>
+              {workspace.active ? (
+                <Chip size="sm" color="success">
+                  Active
+                </Chip>
+              ) : (
+                <Chip size="sm">Inactive</Chip>
+              )}
+            </div>
+
+            <div className="flex justify-between px-4 md:px-6 py-4 border-t border-cultured/20">
+              {userModel?.AccountType === 'ADMIN' && <div className="flex gap-2">
+                <UpdateWorkspaceModal workspace={workspace} />
+                <DeleteWorkspaceModal workspace={workspace} />
+              </div>}
+              <Link
+                className="flex items-center text-sm text-cyan-400 hover:text-cyan-400/80"
+                href={`/workspaces/${workspace.id}/sessions`}
+              >
+                <span>sessions </span>
+                <ChevronRight size={16} />
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+      
+    </section>
   );
 }
