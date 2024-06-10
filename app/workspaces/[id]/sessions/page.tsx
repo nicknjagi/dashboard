daed62242094db0f0b0103fcc503f9ae2f9786c9
Nick <njaginicholas@gmail.com>
2024-06-10 15:21:53 +0300

    update page.tsx

--- app/workspaces/[id]/sessions/page.tsx
+++ app/workspaces/[id]/sessions/page.tsx
@@ -1,6 +1,17 @@
-type Props = {}
+import CreateSessionModal from "@/components/modals/createSessionModal";
+import SessionsList from "@/components/sessionsList";
+
+type Props = {};
+
 export default function Sessions({}: Props) {
+
   return (
-    <div>Sessions</div>
-  )
+    <div>
+      <div className="flex justify-between items-center">
+        <h1 className={"text-xl md:text-2xl font-medium"}>Sessions</h1>
+        <CreateSessionModal />
+      </div>
+      <SessionsList />
+    </div>
+  );
 }
