5b7231017ec0c73f7b167ab0a220570c45ff4a3d
Nick <njaginicholas@gmail.com>
2024-06-11 11:35:36 +0300

    update page.tsx

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,6 +1,15 @@
+import AccountsList from "@/components/accountsList"
+
 type Props = {}
 export default function Accounts({}: Props) {
   return (
-    <div>Accounts</div>
+    <div className="py-2">
+      <div className="flex justify-between items-center">
+      <div className="flex items-end gap-2">
+          <h1 className={"text-xl md:text-2xl font-medium"}>Accounts</h1>
+      </div>
+      </div>
+      <AccountsList />
+    </div>
   )
 }
