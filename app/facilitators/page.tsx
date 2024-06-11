6e86e3a143da2c726c62620e106c60edeecdad2e
Nick <njaginicholas@gmail.com>
2024-06-11 11:35:45 +0300

    update page.tsx

--- app/facilitators/page.tsx
+++ app/facilitators/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Facilitators({}: Props) {
   return (
-    <section className="py-4">
+    <section className="py-2">
       <div className="flex justify-between items-center">
         <h1 className={"text-xl md:text-2xl font-medium"}>Facilitators</h1>
         <SendTokenModal />
