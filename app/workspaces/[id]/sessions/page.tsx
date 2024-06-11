f20cccc5f68e6f1932817082652cc6bc5a5348d7
Nick <njaginicholas@gmail.com>
2024-06-11 11:36:23 +0300

    update page.tsx

--- app/workspaces/[id]/sessions/page.tsx
+++ app/workspaces/[id]/sessions/page.tsx
@@ -1,5 +1,6 @@
 import CreateSessionModal from "@/components/modals/createSessionModal";
 import SessionsList from "@/components/sessionsList";
+import Link from "next/link";
 
 type Props = {};
 
@@ -8,7 +9,10 @@ export default function Sessions({}: Props) {
   return (
     <div className="py-2">
       <div className="flex justify-between items-center">
-        <h1 className={"text-xl md:text-2xl font-medium"}>Sessions</h1>
+      <div className="flex items-end gap-2">
+          <Link href={'/workspaces'} className="text-neutral-500">{"workspaces"}<span className="ml-2 text-xl text-neutral-500">/</span></Link>
+          <h1 className={"text-xl md:text-2xl font-medium"}>Sessions</h1>
+      </div>
         <CreateSessionModal />
       </div>
       <SessionsList />
