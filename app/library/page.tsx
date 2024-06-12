b6458e9c9ee5344bca877e5ac73c8bc48865e9c9
Nick <njaginicholas@gmail.com>
2024-06-12 16:17:40 +0300

    update page.tsx

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,6 +1,16 @@
-type Props = {}
-export default function Library({}: Props) {
+import LibraryList from "@/components/libraryList";
+import AddToLibraryModal from "@/components/modals/addToLibraryModal";
+
+type Props = {};
+
+export default function Facilitators({}: Props) {
   return (
-    <div>Library</div>
-  )
+    <section className="py-2">
+      <div className="flex justify-between items-center">
+        <h1 className={"text-xl md:text-2xl font-medium"}>Library</h1>
+        <AddToLibraryModal />
+      </div>
+      <LibraryList />
+    </section>
+  );
 }
