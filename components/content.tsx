0874628401bce422bce33d3a4074f156b56983d6
Nick <njaginicholas@gmail.com>
2024-06-13 12:22:55 +0300

    feat: create content.tsx

---
+++ components/content.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import parse from "html-react-parser";
+import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+  ModalFooter,
+} from "@nextui-org/modal";
+import React from "react";
+
+type Props = {
+  children: string;
+};
+
+export default function Content({ children }: Props) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        size="sm"
+        onPress={onOpen}
+        variant="flat"
+        className="text-sky-200"      >
+        View
+      </Button>
+      <Modal
+        className="bg-forrestGreen"
+        classNames={{
+          base:'my-4',
+          wrapper:' h-full max-h-screen overflow-y-auto'
+        }}
+        isOpen={isOpen}
+        size="4xl"
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+        scrollBehavior="inside"
+        isKeyboardDismissDisabled={true}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>
+              </ModalHeader>
+              <ModalBody>{parse(children)}</ModalBody>
+              <ModalFooter>
+                <Button onPress={onClose}>Close</Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
