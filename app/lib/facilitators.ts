c473a18248238590770b42eab84652377b728466
Nick <njaginicholas@gmail.com>
2024-06-15 11:50:48 +0300

    update facilitators.ts

--- app/lib/facilitators.ts
+++ app/lib/facilitators.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 import { BASE_URL, pb } from "./utils"
 
-export async function fetchFacilitators() {
+export async function fetchFacilitators(page: number = 1, perPage: number = 10) {
   try {
-    const response = await axios.get(`${BASE_URL}/api/collections/users/records?filter=(AccountType='FACILITATOR')`, {
+    const response = await axios.get(`${BASE_URL}/api/collections/users/records?filter=(AccountType='FACILITATOR')&sort=-created&page=${page}&perPage=${perPage}`, {
       headers: {
         Authorization: `Bearer ${pb.authStore.token}`
       }
