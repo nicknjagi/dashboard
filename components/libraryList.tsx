39a334de369b6f8deb04eecd9795b212e3b13650
Nick <njaginicholas@gmail.com>
2024-06-15 12:14:52 +0300

    update libraryList.tsx

--- components/libraryList.tsx
+++ components/libraryList.tsx
@@ -16,7 +16,8 @@ type Props = {};
 
 export default function LibraryList({}: Props) {
   const [mediaType, setMediaType] = useState("MUSIC");
-  const { data, error, isLoading } = useQuery({
+
+  const { data, error, isLoading, isFetching } = useQuery({
     queryKey: ["libraries", mediaType],
     queryFn: () => getLibraryItems(mediaType),
     placeholderData: keepPreviousData,
@@ -26,7 +27,7 @@ export default function LibraryList({}: Props) {
 
   if (isLoading) return <Loading />;
   if (error) return <p className="mt-10">Something went wrong</p>;
-  
+
   return (
     <section className="mt-6 flex flex-wrap justify-center md:justify-start gap-4 md:gap-6">
       <div className="w-full">
@@ -52,57 +53,61 @@ export default function LibraryList({}: Props) {
           })}
         </ButtonGroup>
       </div>
-      {data.items.map((library: LibraryItem) => {
-        return library.type === "MUSIC" ? (
-          <div
-            key={library.id}
-            className="p-4 w-full md:max-w-sm border shadow-md border-cultured/20 rounded-lg bg-forrestGreen"
-          >
-            <h2 className="capitalize mb-2">
-              {library.description.toLowerCase()}
-            </h2>
-            <audio className="" controls>
-              <source src={library.link} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-            <div className="flex gap-2 mt-4">
-              <UpdateLibraryItemModal libraryItem={library} />
-              <DeleteLibraryItemModal libraryItem={library} />
-            </div>
-          </div>
-        ) : library.type === "VIDEO" ? (
-          <div
-            key={library.id}
-            className="p-4 w-full md:max-w-sm border shadow bg-forrestGreen border-cultured/20 rounded-xl"
-          >
-            <h2 className="text-xl capitalize">{library.name}</h2>
-            <p>{library.description}</p>
-            <video controls>
-              <source src={library.link} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-
-            <div className="flex gap-2 mt-4">
-              <UpdateLibraryItemModal libraryItem={library} />
-              <DeleteLibraryItemModal libraryItem={library} />
-            </div>
-          </div>
-        ) : (
-          <div
-            key={library.id}
-            className="p-4 w-full md:max-w-sm border shadow bg-forrestGreen border-cultured/20 rounded-xl"
-          >
-            <h2 className="text-xl capitalize">{library.description}</h2>
-            <div className="flex justify-between items-center mt-4">
-              <div className="flex gap-2">
+      {isFetching ? (
+        <Loading />
+      ) : (
+        data.items.map((library: LibraryItem) => {
+          return library.type === "MUSIC" ? (
+            <div
+              key={library.id}
+              className="p-4 w-full md:max-w-sm border shadow-md border-cultured/20 rounded-lg bg-forrestGreen"
+            >
+              <h2 className="capitalize mb-2">
+                {library.description.toLowerCase()}
+              </h2>
+              <audio className="" controls>
+                <source src={library.link} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+              <div className="flex gap-2 mt-4">
                 <UpdateLibraryItemModal libraryItem={library} />
                 <DeleteLibraryItemModal libraryItem={library} />
               </div>
-              <Content>{library.content}</Content>
             </div>
-          </div>
-        );
-      })}
+          ) : library.type === "VIDEO" ? (
+            <div
+              key={library.id}
+              className="p-4 w-full md:max-w-sm border shadow bg-forrestGreen border-cultured/20 rounded-xl"
+            >
+              <h2 className="text-xl capitalize">{library.name}</h2>
+              <p>{library.description}</p>
+              <video controls>
+                <source src={library.link} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+
+              <div className="flex gap-2 mt-4">
+                <UpdateLibraryItemModal libraryItem={library} />
+                <DeleteLibraryItemModal libraryItem={library} />
+              </div>
+            </div>
+          ) : (
+            <div
+              key={library.id}
+              className="p-4 w-full md:max-w-sm border shadow bg-forrestGreen border-cultured/20 rounded-xl"
+            >
+              <h2 className="text-xl capitalize">{library.description}</h2>
+              <div className="flex justify-between items-center mt-4">
+                <div className="flex gap-2">
+                  <UpdateLibraryItemModal libraryItem={library} />
+                  <DeleteLibraryItemModal libraryItem={library} />
+                </div>
+                <Content>{library.content}</Content>
+              </div>
+            </div>
+          );
+        })
+      )}
     </section>
   );
 }
