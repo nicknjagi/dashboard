256d43f4ee1380c1558993cf2a0ac0dd69bdec17
Nick <njaginicholas@gmail.com>
2024-06-24 14:53:38 +0300

    update usersList.tsx

--- components/usersList.tsx
+++ components/usersList.tsx
@@ -1,40 +1,16 @@
 "use client";
 
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  getKeyValue,
-} from "@nextui-org/table";
 import { User as TUser } from "@/types";
-import { BadgeCheck } from "lucide-react";
+import { BadgeCheck, CircleAlert } from "lucide-react";
 import Loading from "./loading";
 import { User } from "@nextui-org/user";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { DateTime } from "luxon";
 import { fetchUsers } from "@/app/lib/users";
 import { Pagination } from "@nextui-org/pagination";
 import { useState } from "react";
+import { Tooltip } from "@nextui-org/tooltip";
 
 type Props = {};
-
-const columns = [
-  {
-    key: "name",
-    label: "NAME",
-  },
-  {
-    key: "verified",
-    label: "VERIFIED",
-  },
-  {
-    key: "created",
-    label: "CREATED",
-  },
-];
 
 export default function UsersList({}: Props) {
   const [page, setPage] = useState(1);
@@ -55,79 +31,87 @@ export default function UsersList({}: Props) {
   }
 
   return (
-    <div className="mt-6 overflow-x-auto rounded-2xl shadow-md max-w-fit">
-      <Table
-        bottomContent={
-          <div className="flex w-full justify-center">
-            <Pagination
-              isCompact
-              showControls
-              showShadow
-              page={page}
-              total={data.totalPages}
-              onChange={setPage}
-              classNames={{
-                cursor:"bg-cultured/20 shadow-cultured/10 text-white font-bold"
-              }}
-            />
+    <div className="mt-6 flex flex-wrap justify-center md:justify-start gap-4 ">
+      {data.items.length > 0 ? (
+        data.items.map((user: TUser) => (
+          <div
+            key={user.id}
+            className="p-4 w-full md:max-w-sm border border-cultured/10 rounded-lg bg-forrestGreen"
+          >
+            <div className="flex items-center gap-4">
+              <User
+                avatarProps={{
+                  radius: "lg",
+                  src: user?.avatar
+                    ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/files/users/${user.id}/${user?.avatar}`
+                    : "/user-round.svg",
+                }}
+                description={user.email}
+                name={user.name}
+              >
+                {user.email}
+              </User>
+              {user.verified ? (
+                <Tooltip
+                  content="verified"
+                  className="capitalize"
+                  classNames={{
+                    base:["rounded-lg bg-[#48b446]"],
+                    content: [
+                      "bg-transparent",
+                      "px-2",
+                      "text-sm text-black",
+                    ],
+                  }}
+                >
+                  <BadgeCheck
+                    color="#48b446"
+                    className="ml-auto cursor-pointer"
+                  />
+                </Tooltip>
+              ) : (
+                <Tooltip
+                  content="unverified"
+                  className="capitalize"
+                  classNames={{
+                    base:["rounded-lg bg-[#4d0000]"],
+                    content: [
+                      "bg-transparent",
+                      "px-2",
+                      "text-sm text-white",
+                    ],
+                  }}
+                >
+                  <CircleAlert
+                    color="#4d0000"
+                    className="ml-auto cursor-pointer"
+                  />
+                </Tooltip>
+              )}
+            </div>
+            {/* <p className="mt-2 text-sm text-gray-600">
+              Created:{" "}
+              {DateTime.fromISO(user.created.replace(" ", "T"), {
+                zone: "utc",
+              }).toFormat("dd/MM/yyyy hh:mm a")}
+            </p> */}
           </div>
-        }
-        classNames={{
-          wrapper: [
-            "bg-background border border-cultured/10 w-full min-w-[640px] max-w-fit",
-          ],
-          th: ["bg-cultured/10 "],
-        }}
-        aria-label="Facilitators list"
-      >
-        <TableHeader columns={columns}>
-          {(column) => (
-            <TableColumn key={column.key}>{column.label}</TableColumn>
-          )}
-        </TableHeader>
-        {data.items.length > 0 ? (
-          <TableBody items={data.items}>
-            {(facilitator: TUser) => (
-              <TableRow key={facilitator.username}>
-                {(columnKey) => (
-                  <TableCell>
-                    {columnKey === "verified" ? (
-                      facilitator.verified ? (
-                        <span className="flex justify-center lg:justify-start">
-                          <BadgeCheck color="#48b446" />
-                        </span>
-                      ) : (
-                        <span className="block text-center lg:text-start">
-                          -
-                        </span>
-                      )
-                    ) : columnKey === "name" ? (
-                      <User
-                        avatarProps={{
-                          radius: "lg",
-                          src: facilitator?.avatar
-                            ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/files/users/${facilitator.id}/${facilitator?.avatar}`
-                            : "/user-round.svg",
-                        }}
-                        description={facilitator.email}
-                        name={facilitator.name}
-                      >
-                        {facilitator.email}
-                      </User>
-                    ) : columnKey === "created" ? (
-                      <span>{DateTime.fromISO(facilitator.created.replace(" ", "T"), { zone: 'utc' }).toFormat("dd/MM/yyyy hh:mm a")}</span>
-                    ) : (
-                      getKeyValue(facilitator, columnKey)
-                    )}
-                  </TableCell>
-                )}
-              </TableRow>
-            )}
-          </TableBody>
-        ) : (
-          <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
-        )}
-      </Table>
+        ))
+      ) : (
+        <p className="mt-10">No users to display.</p>
+      )}
+      <div className="w-full flex justify-start mt-6">
+        <Pagination
+          isCompact
+          showControls
+          page={page}
+          total={data.totalPages}
+          onChange={setPage}
+          classNames={{
+            cursor: "bg-forrestGreen shadow-sm shadow-cultured/10 text-white font-bold",
+          }}
+        />
+      </div>
     </div>
   );
 }
