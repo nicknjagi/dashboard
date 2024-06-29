fcac7210b100075e61a995ef61cff60dcf923944
Nick <njaginicholas@gmail.com>
2024-06-29 12:52:26 +0300

    feat: create videoItem.tsx

---
+++ components/videoItem.tsx
@@ -0,0 +1,24 @@
+import { LibraryItem } from "@/types";
+import UpdateLibraryItemModal from "./modals/updateLibraryItemModal";
+import DeleteLibraryItemModal from "./modals/deleteLibraryItemModal";
+
+type Props = {
+  libraryItem: LibraryItem;
+};
+
+export default function VideoItem({ libraryItem }: Props) {
+  return (
+    <div className="p-4 w-full md:max-w-sm border shadow bg-forrestGreen border-cultured/20 rounded-xl">
+      <h3 className="text-xl capitalize">{libraryItem.name}</h3>
+      <p>{libraryItem.description}</p>
+      <video controls>
+        <source src={libraryItem.link} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="flex gap-2 mt-4">
+        <UpdateLibraryItemModal libraryItem={libraryItem} />
+        <DeleteLibraryItemModal libraryItem={libraryItem} />
+      </div>
+    </div>
+  );
+}
