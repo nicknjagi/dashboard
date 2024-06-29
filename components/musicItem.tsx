d0f71aab49b85a9a9680f8e3460e02f45f64f8b8
Nick <njaginicholas@gmail.com>
2024-06-29 12:51:56 +0300

    feat: create musicItem.tsx

---
+++ components/musicItem.tsx
@@ -0,0 +1,25 @@
+import { LibraryItem } from "@/types";
+import UpdateLibraryItemModal from "./modals/updateLibraryItemModal";
+import DeleteLibraryItemModal from "./modals/deleteLibraryItemModal";
+
+type Props = {
+  libraryItem: LibraryItem;
+};
+
+export default function MusicItem({ libraryItem }: Props) {
+  return (
+    <div className="p-4 w-full md:max-w-sm border shadow-md border-cultured/20 rounded-lg bg-forrestGreen">
+      <h2 className="capitalize mb-2">
+        {libraryItem.description.toLowerCase()}
+      </h2>
+      <audio className="" controls>
+        <source src={libraryItem.link} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <div className="flex gap-2 mt-4">
+        <UpdateLibraryItemModal libraryItem={libraryItem} />
+        <DeleteLibraryItemModal libraryItem={libraryItem} />
+      </div>
+    </div>
+  );
+}
