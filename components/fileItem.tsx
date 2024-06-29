56c39bacef06fe6161f3496ae9564f1a84fb7695
Nick <njaginicholas@gmail.com>
2024-06-29 12:51:41 +0300

    feat: create fileItem.tsx

---
+++ components/fileItem.tsx
@@ -0,0 +1,24 @@
+import { LibraryItem } from "@/types";
+import UpdateFileForm from "./forms/updateFileForm";
+import DeleteLibraryItemModal from "./modals/deleteLibraryItemModal";
+import Content from "./content";
+import parse from "html-react-parser";
+
+type Props = {
+  libraryItem: LibraryItem;
+};
+
+export default function FileItem({ libraryItem }: Props) {
+  return (
+    <div className="p-4 w-full md:max-w-sm border shadow bg-forrestGreen border-cultured/20 rounded-xl">
+      <div className="overflow-hidden h-56 text-ellipsis prose prose-invert">{parse(libraryItem.content)}</div>
+      <div className="flex justify-between items-center mt-4">
+        <div className="flex gap-2">
+          <UpdateFileForm libraryItem={libraryItem} />
+          <DeleteLibraryItemModal libraryItem={libraryItem} />
+        </div>
+        <Content libraryItem={libraryItem}>{libraryItem.content}</Content>
+      </div>
+    </div>
+  );
+}
