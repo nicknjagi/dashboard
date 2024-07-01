a2f5748e54e8ad14160420711bb75e70a0330589
Nick <njaginicholas@gmail.com>
2024-07-01 08:47:42 +0300

    update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import PocketBase from 'pocketbase'
+import { v4 as uuidv4 } from 'uuid';
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 // const BASE_URL = 'https://mymindcapsule.pockethost.io';
@@ -17,4 +18,10 @@ export async function sendEmail(email: string) {
     throw new Error('Error sending invite')
   }
 }
+
+export const generateJitsiLink = () => {
+  const randomString = uuidv4();
+  return `https://meet.jit.si/${randomString}`;
+};
+
 
