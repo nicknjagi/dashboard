79cb28acb0301c16353642eed9080d7aa85afcdf
Nick <njaginicholas@gmail.com>
2024-07-01 08:46:55 +0300

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-
+import ActiveUsersCountCard from "@/components/activeUsersCountCard";
+import WorkspacesCountCard from "@/components/workspacesCountCard";
 
 export default function Home() {
-	return (
-		<section className="">
-			<h1 className={'text-3xl font-semibold'}>Dashboard</h1>
-		</section>
-	);
+  return (
+    <section className="">
+      <h1 className={"text-xl md:text-2xl font-medium mb-6"}>Dashboard</h1>
+      <div className="flex flex-col md:flex-row flex-wrap gap-4 md:gap-6">
+        <WorkspacesCountCard />
+        <ActiveUsersCountCard />
+      </div>
+    </section>
+  );
 }
