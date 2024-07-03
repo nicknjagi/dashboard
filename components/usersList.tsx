2664f9cab2940344b5476d2c11e901a57a91ca7e
Nick <njaginicholas@gmail.com>
2024-07-03 09:07:36 +0300

    update usersList.tsx

--- components/usersList.tsx
+++ components/usersList.tsx
@@ -1,100 +1,13 @@
 "use client";
 
-import { User as TUser } from "@/types";
-import { BadgeCheck, CircleAlert } from "lucide-react";
-import Loading from "./loading";
-import { User } from "@nextui-org/user";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { fetchUsers } from "@/app/lib/users";
-import { Pagination } from "@nextui-org/pagination";
-import { useState } from "react";
-import { Tooltip } from "@nextui-org/tooltip";
+import UserInfo from "./userInfo";
 
 type Props = {};
 
 export default function UsersList({}: Props) {
-  const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
-
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["users", page],
-    queryFn: () => fetchUsers(page, perPage),
-    placeholderData: keepPreviousData,
-  });
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <p className="mt-10">Error fetching users</p>;
-  }
-
   return (
-    <div className="mt-6 flex flex-wrap justify-center md:justify-start gap-4 ">
-      {data.items.length > 0 ? (
-        data.items.map((user: TUser) => (
-          <div
-            key={user.id}
-            className="p-4 w-full md:max-w-sm border border-cultured/10 rounded-lg bg-forrestGreen"
-          >
-            <div className="flex items-center gap-4">
-              <User
-                avatarProps={{
-                  radius: "lg",
-                  src: user?.avatar
-                    ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/files/users/${user.id}/${user?.avatar}`
-                    : "/user-round.svg",
-                }}
-                description={user.email}
-                name={user.name}
-              >
-                {user.email}
-              </User>
-              <div className="ml-auto cursor-pointer scale-75">
-                {user.verified ? (
-                  <Tooltip
-                    content="verified"
-                    className="capitalize"
-                    classNames={{
-                      base: ["rounded-lg", "scale-50"],
-                      content: ["px-2", "text-[12px]"],
-                    }}
-                  >
-                    <BadgeCheck color="#48b446" />
-                  </Tooltip>
-                ) : (
-                  <Tooltip
-                    content="unverified"
-                    className="capitalize"
-                    classNames={{
-                      base: ["rounded-lg", "scale-50"],
-                      content: ["px-2", "text-[12px]"],
-                    }}
-                  >
-                    <CircleAlert color="#ff4d4d" />
-                  </Tooltip>
-                )}
-              </div>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p className="mt-10">No users to display.</p>
-      )}
-      <div className="w-full flex justify-start mt-6">
-        <Pagination
-          isCompact
-          showControls
-          page={page}
-          total={data.totalPages}
-          onChange={setPage}
-          classNames={{
-            cursor:
-              "bg-forrestGreen border border-cultured/20 text-white font-bold",
-          }}
-        />
-      </div>
+    <div className="mt-6">
+      <UserInfo />
     </div>
   );
 }
