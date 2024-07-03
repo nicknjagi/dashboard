5dd5e8cb155e839c3ba177c4f3f8e6da3f504135
Nick <njaginicholas@gmail.com>
2024-07-03 09:06:59 +0300

    add loading state

--- components/activeUsersCountCard.tsx
+++ components/activeUsersCountCard.tsx
@@ -4,29 +4,46 @@ import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { ChevronRight } from "lucide-react";
 import { activeUserCount } from "@/app/lib/accounts";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 type Props = {};
 
 export default function ActiveUsersCountCard({}: Props) {
-  const { data: asCount, error, isLoading} = useQuery({
+  const {
+    data: asCount,
+    error,
+    isLoading,
+  } = useQuery({
     queryKey: ["activeUsersCount"],
     queryFn: activeUserCount,
   });
 
   return (
     <div className="card w-full max-w-xs p-4">
-      <h2 className="mb-2 text-neutral-300">Active Users</h2>
-      <div className="flex items-end justify-between ">
-        <span className="text-3xl md:text-4xl font-medium">
-          {asCount?.totalItems}
-        </span>
-        <Link
-          href="/accounts"
-          className="grid place-content-center w-8 h-8 bg-cultured/5 hover:bg-cultured/10 rounded-full"
-        >
-          <ChevronRight />
-        </Link>
-      </div>
+      {isLoading ? (
+        <SkeletonTheme baseColor="#014d4d" highlightColor="#026666">
+          <Skeleton width={170} height={20} />
+          <div className="flex justify-between items-end mt-2">
+            <Skeleton width={40} height={40} />
+            <Skeleton circle width={32} height={32} />
+          </div>
+        </SkeletonTheme>
+      ) : (
+        <>
+          <h2 className="mb-2 text-neutral-300">Active Users</h2>
+          <div className="flex items-end justify-between ">
+            <span className="text-3xl md:text-4xl font-medium">
+              {asCount?.totalItems}
+            </span>
+            <Link
+              href="/accounts"
+              className="grid place-content-center w-8 h-8 bg-cultured/5 hover:bg-cultured/10 rounded-full"
+            >
+              <ChevronRight />
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- components/workspacesCountCard.tsx
+++ components/workspacesCountCard.tsx
@@ -4,29 +4,47 @@ import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { ChevronRight } from "lucide-react";
 import { workspacesCount } from "@/app/lib/workspaces";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 type Props = {};
 
 export default function WorkspacesCountCard({}: Props) {
-  const { data: wsCount, error, isLoading} = useQuery({
+  const {
+    data: wsCount,
+    error,
+    isLoading,
+  } = useQuery({
     queryKey: ["workspacesCount"],
     queryFn: workspacesCount,
   });
 
   return (
     <div className="card w-full max-w-xs p-4">
-      <h2 className="mb-2 text-neutral-300">Total workspaces</h2>
-      <div className="flex items-end justify-between ">
-        <span className="text-3xl md:text-4xl font-medium">
-          {wsCount?.totalItems}
-        </span>
-        <Link
-          href="/workspaces"
-          className="grid place-content-center w-8 h-8 bg-cultured/5 hover:bg-cultured/10 rounded-full"
-        >
-          <ChevronRight />
-        </Link>
-      </div>
+      {isLoading ? (
+        <SkeletonTheme baseColor="#014d4d" highlightColor="#026666">
+          <Skeleton width={170} height={20} />
+          <div className="flex justify-between items-end mt-2">
+            <Skeleton width={40} height={40} />
+            <Skeleton circle width={32} height={32} />
+          </div>
+        </SkeletonTheme>
+      ) : (
+        <>
+          <h2 className="mb-2 text-neutral-300">Total workspaces</h2>
+          <div className="flex items-end justify-between ">
+            <span className="text-3xl md:text-4xl font-medium">
+              {wsCount?.totalItems}
+            </span>
+            <Link
+              href="/workspaces"
+              className="grid place-content-center w-8 h-8 bg-cultured/5 hover:bg-cultured/10 rounded-full"
+            >
+              <ChevronRight />
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
