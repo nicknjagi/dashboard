2443ec8b8dee946abaad02c3b44a6cf24a9b824b
Nick <njaginicholas@gmail.com>
2024-07-05 09:43:27 +0300

    update userInfo.tsx

--- components/userInfo.tsx
+++ components/userInfo.tsx
@@ -1,71 +1,33 @@
 "use client";
 
 import { getUserData } from "@/app/lib/users";
-import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
-import { User } from "@nextui-org/user";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-export function UserInfo() {
-  const [userId, setUserId] = useState("");
-  const [submittedUserId, setSubmittedUserId] = useState("");
-
-  const { data, error, isLoading, refetch } = useQuery({
-    queryKey: ["getUserData", submittedUserId],
-    queryFn: () => getUserData(submittedUserId),
-    enabled: !!submittedUserId, 
+export function UserInfo({ userId }: { userId: string }) {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["getUserData", userId],
+    queryFn: () => getUserData(userId),
     refetchOnMount: true,
     staleTime: 0,
   });
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setSubmittedUserId(userId);
-    if (submittedUserId === userId) {
-      refetch();
-    }
-  };
+  console.log(data);
 
   return (
     <div>
-      <form className="my-4" onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          variant={"bordered"}
-          classNames={{
-            inputWrapper: "bg-forrestGreen border border-cultured/20 max-w-md",
-          }}
-          label="User ID"
-          value={userId}
-          onValueChange={setUserId}
-        />
-        <Button size="sm" className="btn mt-3" type="submit">Search</Button>
-      </form>
-
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error.message}</p>}      
+      {isLoading && (
+        <SkeletonTheme baseColor="#014d4d" highlightColor="#026666">
+          <Skeleton width={150} height={20} />
+          <Skeleton width={200} height={20} />
+        </SkeletonTheme>
+      )}
+      {error && <p>{error.message}</p>}
       {data?.user && (
         <div>
-          <h1>User Data</h1>
-          <div
-            className="p-4 w-full md:max-w-sm border border-cultured/10 rounded-lg bg-forrestGreen"
-          >
-            <div className="flex items-center gap-4">
-              <User
-                avatarProps={{
-                  radius: "lg",
-                  src: data.user?.image_url
-                    ? data.user?.image_url
-                    : "/user-round.svg",
-                }}
-                description={data.user.email_addresses[0].email_address}
-                name={`${data.user.first_name} ${data.user.last_name}`}
-              >
-                {data.user.email_addresses[0].email_address}
-              </User>
-            </div>
-          </div>
+          <p>{`${data.user.first_name} ${data.user.last_name}`}</p>
+          <p>{data.user.email_addresses[0].email_address}</p>
         </div>
       )}
     </div>
