6884c1f263a7a5e86ef4a3441d3e7b7cfe81dd9f
Nick <njaginicholas@gmail.com>
2024-07-05 09:40:23 +0300

    update users.ts

--- app/lib/users.ts
+++ app/lib/users.ts
@@ -1,17 +1,33 @@
 import axios from "axios"
-import { BASE_URL, pb } from "./utils"
+import { pb } from "./utils"
 
 export async function fetchUsers(page: number = 1, perPage: number = 10) {
   try {
-    const response = await axios.get(`${BASE_URL}/api/collections/users/records?sort=-created&page=${page}&perPage=${perPage}`, {
+    const response = await axios.get(`/api/get-users?page=${page}&perPage=${perPage}`, {
       headers: {
         Authorization: `Bearer ${pb.authStore.token}`
       }
     })
+
     return response.data
   } catch (error) {
     console.log(error)
     throw new Error('Error fetching users')
+  }
+}
+
+export async function fetchTotalUsers() {
+  try {
+    const response = await axios.get('/api/get-users-total', {
+      headers: {
+        Authorization: `Bearer ${pb.authStore.token}`,
+      },
+    });
+
+    return response.data.total_count;
+  } catch (error) {
+    console.error('Error fetching total users:', error);
+    throw new Error('Error fetching total users');
   }
 }
 
@@ -25,17 +41,7 @@ export async function getUserData(userId: string) {
     })
     return response.data;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error('Error response data:', error.response?.data.error);
-      if(error.response?.status === 404){
-        throw new Error('User not found')
-      } else if(error.response?.status === 401){
-        throw new Error('Unauthorized')
-      }
-      throw new Error(error.response?.data)
-    } else {
-      console.error('Error message:', error);
-      throw new Error('Error fetching user data');
-    }
+    console.log('Error: ', error);
+    throw new Error('Error fetching user data');
   }
 }
