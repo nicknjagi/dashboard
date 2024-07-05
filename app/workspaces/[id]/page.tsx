1e3e7b3b60a0750c51d71a322b46e3cfcd811fc2
Nick <njaginicholas@gmail.com>
2024-07-05 09:42:08 +0300

    update page.tsx

--- app/workspaces/[id]/page.tsx
+++ app/workspaces/[id]/page.tsx
@@ -1,14 +1,22 @@
-import CreateSessionModal from "@/components/modals/createSessionModal";
+'use client'
+
+import { getWorkspace } from "@/app/lib/workspaces";
 import SessionsList from "@/components/sessionsList";
-import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 type Props = {};
 
 export default function Sessions({}: Props) {
-
+  const { id } = useParams();
+  const { data: workspace } = useQuery({
+    queryKey: ["getWorkspace"],
+    queryFn: () => getWorkspace(id as string),
+  });
+  
   return (
     <div className="py-2">
-      <SessionsList />
+      <SessionsList workspace={workspace}/>
     </div>
   );
 }
