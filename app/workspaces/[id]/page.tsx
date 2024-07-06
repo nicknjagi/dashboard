73fa9fb20093148abdd2280379107d068e9d8681
Nick <njaginicholas@gmail.com>
2024-07-06 09:02:52 +0300

    update page.tsx

--- app/workspaces/[id]/page.tsx
+++ app/workspaces/[id]/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { getWorkspace } from "@/app/lib/workspaces";
+import Loading from "@/components/loading";
 import SessionsList from "@/components/sessionsList";
+import WorkspaceUsers from "@/components/workspaceUsers";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 
@@ -9,14 +11,23 @@ type Props = {};
 
 export default function Sessions({}: Props) {
   const { id } = useParams();
-  const { data: workspace } = useQuery({
+  const { data: workspace, isLoading, error } = useQuery({
     queryKey: ["getWorkspace"],
     queryFn: () => getWorkspace(id as string),
   });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <p className="mt-10">Error fetching data</p>;
+  }
   
   return (
     <div className="py-2">
       <SessionsList workspace={workspace}/>
+      <WorkspaceUsers accounts={workspace?.expand?.users} />
     </div>
   );
 }
