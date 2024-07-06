56a0927811794d107eafa11db3be45cc662ab0c5
Nick <njaginicholas@gmail.com>
2024-07-06 09:01:50 +0300

    update page.tsx

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,10 +1,11 @@
 import CreateAccount from "@/components/forms/createAccountForm";
+import Loading from "@/components/loading";
 import { Suspense } from "react";
 
 type Props = {};
 export default function CreateAccountPage({}: Props) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <CreateAccount />
     </Suspense>
   );
