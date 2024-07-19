a2f95f8bff2b85302bf80a719c4f4d3868369b74
Nick <njaginicholas@gmail.com>
2024-07-19 14:10:09 +0300

    update workspaceUsers.tsx

--- components/workspaceUsers.tsx
+++ components/workspaceUsers.tsx
@@ -5,17 +5,25 @@ import UserInfo from "./userInfo";
 
 type Props = {
   accounts: [];
+  workspaceId: string;
 };
 
-export default function WorkspaceUsers({ accounts }: Props) {
+export default function WorkspaceUsers({ accounts, workspaceId }: Props) {
   return (
     <div className="mt-6 ">
       <h3 className="my-2 font-medium text-lg">Users</h3>
       <div className="flex flex-wrap gap-4">
         {accounts?.length > 0 ? (
           accounts.map((account: Account) => (
-            <div key={account.id} className="flex p-4 pb-2 w-full min-w-[240px] md:max-w-sm card">
-              <UserInfo userId={account.userId} accountId={account.id} />
+            <div
+              key={account.id}
+              className="flex p-4 pb-2 w-full min-w-[240px] md:max-w-sm card"
+            >
+              <UserInfo
+                userId={account.userId}
+                accountId={account.id}
+                workspaceId={workspaceId}
+              />
             </div>
           ))
         ) : (
